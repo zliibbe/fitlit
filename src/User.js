975d3a3a8cddfc81c5a987116c300865d4bb9791
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-02-08 15:12:56 -0700

    Add returnFirstName method

--- src/User.js
+++ src/User.js
@@ -1,12 +1,17 @@
 class User {
   constructor(userData) {
-    this.userDataId = userData.id;
+    this.id = userData.id;
     this.name = userData.name;
     this.address = userData.address;
     this.email = userData.email;
     this.strideLength = userData.strideLength;
     this.dailyStepGoal = userData.dailyStepGoal;
     this.friends = userData.friends;
+  }
+
+  returnFirstName() {
+    const name = this.name.split(' ');
+    return name[0];
   }
 }
 
