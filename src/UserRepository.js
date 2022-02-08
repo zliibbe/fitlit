a8683e60362cc654ecc0222e2ee0b362133e742f
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-02-07 22:06:56 -0700

    Add constructor and two methods

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,5 +1,19 @@
-class UserRepository {
+import User from "./User";
 
+class UserRepository {
+  constructor(userData) {
+    this.users = userData.map(user => {
+      return new User(user)
+    });
+  }
+
+  getUserId() {
+
+  }
+
+  getAvgStepGoal() {
+
+  }
 }
 
 export default UserRepository;
