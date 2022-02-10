09763339737690f4c32467607f4af56dc3d79395
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-09 19:37:48 -0700

    Create dynamic api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,22 +2,56 @@
 console.log('I will be a fetch request!')
 
 // fetch users from herokuapp - array of 50 objects
-const users = fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+fetch("https://fitlit-api.herokuapp.com/api/v1/users")
   .then(response => response.json())
   .then(data => console.log(data.userData));
+
+const users;
+
+let fetchUserData = () => {
+  fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+  .then(response => response.json())
+  .then(data => users = data.userData)
+}
 
 // fetch sleep data from herokuapp
 fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
   .then(response => response.json())
   .then(data => console.log(data.sleepData));
 
-// fetch activity data from herokuapp 
+
+const sleepData;
+
+let fetchSleepData = () => {
+  fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
+  .then(response => response.json())
+  .then(data => sleepData = data.sleepData)
+}
+// fetch activity data from herokuapp
 fetch("https://fitlit-api.herokuapp.com/api/v1/activity")
   .then(response => response.json())
   .then(data => console.log(data.activityData));
 
-// fetch activity data from herokuapp 
+const activityData;
+
+let fetchActivityData = () => {
+  fetch("https://fitlit-api.herokuapp.com/api/v1/activity")
+  .then(response => response.json())
+  .then(data => activityData = data.activityData)
+}
+
+// fetch hydration data from herokuapp
 fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
 .then(response => response.json())
 .then(data => console.log(data.hydrationData));
 
+
+const hydrationData;
+
+let fetchhydrationData = () => {
+  fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
+  .then(response => response.json())
+  .then(data => activityData = data.hydrationData)
+}
+
+export default apiCalls; 
