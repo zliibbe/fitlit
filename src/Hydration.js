c5a51853b97c2334171c79e874eeae25d5a678bd
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-02-11 16:18:53 -0700

    Add constructor

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,6 +1,11 @@
 class Hydration{
-  constructor() {
-    
+  constructor(data, id) {
+    this.hydrationData = data.filter((data) => {
+      if(data.userID === id) {
+        return data
+      }
+    })
+    this.userID = id;
   }
 }
 
