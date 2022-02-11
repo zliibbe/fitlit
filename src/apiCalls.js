aeda903207da1a21568fe10af47e6e1c806411e7
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-11 14:02:46 -0700

    Remove lines from fetch calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,57 +1,17 @@
 // Your fetch requests will live here!
-console.log('I will be a fetch request!')
-
-// fetch users from herokuapp - array of 50 objects
-fetch("https://fitlit-api.herokuapp.com/api/v1/users")
-  .then(response => response.json())
-  .then(data => console.log(data.userData));
-
-const users;
-
-let fetchUserData = () => {
-  fetch("https://fitlit-api.herokuapp.com/api/v1/users")
-  .then(response => response.json())
-  .then(data => users = data.userData)
-}
-
-// fetch sleep data from herokuapp
-fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
-  .then(response => response.json())
-  .then(data => console.log(data.sleepData));
 
 
-const sleepData;
+const fetchUserData = () => {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+  .then(response => response.json())}
 
-let fetchSleepData = () => {
+const fetchSleepData = () => {
   fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
-  .then(response => response.json())
-  .then(data => sleepData = data.sleepData)
-}
-// fetch activity data from herokuapp
-fetch("https://fitlit-api.herokuapp.com/api/v1/activity")
-  .then(response => response.json())
-  .then(data => console.log(data.activityData));
+  .then(response => response.json())}
 
-const activityData;
-
-let fetchActivityData = () => {
-  fetch("https://fitlit-api.herokuapp.com/api/v1/activity")
-  .then(response => response.json())
-  .then(data => activityData = data.activityData)
-}
-
-// fetch hydration data from herokuapp
-fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
-.then(response => response.json())
-.then(data => console.log(data.hydrationData));
-
-
-const hydrationData;
-
-let fetchhydrationData = () => {
+const fetchHydrationData = () => {
   fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
-  .then(response => response.json())
-  .then(data => activityData = data.hydrationData)
-}
+  .then(response => response.json())}
 
-export default apiCalls; 
+
+export {fetchUserData, fetchSleepData, fetchHydrationData};
