e2dcf893dc4e234b12aaed7e4742f74b2a6c2156
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-02-13 13:59:07 -0700

    Add constructor properties

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,6 +1,11 @@
 class Sleep{
-  constructor() {
-    
+  constructor(data, id) {
+    this.sleepData = data.filter((data) => {
+      if(data.userID === id) {
+        return data
+      }
+    })
+    this.userID = id;
   }
 }
 export default Sleep
