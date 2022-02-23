361ac962575181b03440f25800e68acf2e67bdf2
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-22 22:20:09 -0700

    Add property and method to sleep class for all user data

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,11 +1,19 @@
 class Sleep{
   constructor(data, id) {
+    this.allData = data;
     this.sleepData = data.filter((data) => {
       if(data.userID === id) {
         return data
       }
     })
     this.userID = id;
+  }
+  totalAvgSleepQuality() {
+    let totalSleepQuality =  this.allData.reduce((acc, data) => {
+      acc += data.sleepQuality
+      return acc
+    }, 0)
+    return Math.round((totalSleepQuality / this.allData.length) * 100) / 100
   }
 }
 
