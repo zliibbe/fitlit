f4b96553326d2e58777b34cc04d4694d5242d851
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-23 16:34:21 -0700

    Add charts for Sleep data

--- src/Chart.js
+++ src/Chart.js
@@ -52,4 +52,109 @@ const weeklyHydration = new Chart(weeklyHydrationChart, {
     }
 });
 
-export {dailyHydration, weeklyHydration}
+const dailySleepQualityChart = document.getElementById('daily-sleep-quality-chart');
+const dailySleepQual = new Chart(dailySleepQualityChart, {
+    type: 'bar',
+    data: {
+        labels: ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
+        datasets: [{
+            label: 'Quality Per Day',
+            data: [],
+            backgroundColor: [
+                'blue'
+            ],
+            borderColor: [
+                'black'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const dailySleepQuantityChart = document.getElementById('daily-sleep-quantity-chart');
+const dailySleepQuan = new Chart(dailySleepQuantityChart, {
+    type: 'bar',
+    data: {
+        labels: ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
+        datasets: [{
+            label: 'Hours Per Day',
+            data: [],
+            backgroundColor: [
+                'blue'
+            ],
+            borderColor: [
+                'black'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const weeklySleepQualityChart = document.getElementById('weekly-sleep-quality-chart');
+const weeklySleepQual = new Chart(weeklySleepQualityChart, {
+    type: 'bar',
+    data: {
+        labels: ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
+        datasets: [{
+            label: 'Quality Per Day',
+            data: [],
+            backgroundColor: [
+                'blue'
+            ],
+            borderColor: [
+                'black'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const weeklySleepQuantityChart = document.getElementById('weekly-sleep-quantity-chart');
+const weeklySleepQuan = new Chart(weeklySleepQuantityChart, {
+    type: 'bar',
+    data: {
+        labels: ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
+        datasets: [{
+            label: 'Hours Per Day',
+            data: [],
+            backgroundColor: [
+                'blue'
+            ],
+            borderColor: [
+                'black'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+export {dailyHydration, weeklyHydration, dailySleepQual, dailySleepQuan, weeklySleepQual, weeklySleepQuan}
