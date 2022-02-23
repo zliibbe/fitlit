4823d98e49035dd6963d170ba6e9c893ddbf5c31
Zach Liibbe <zliibbe@gmail.com>
2022-02-22 22:18:31 -0700

    Add arrow image for dropdowns

--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,7 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/user-icon.png';
+import './images/dropdown.png';
 import {fetchUserData, fetchSleepData, fetchHydrationData} from './apiCalls.js';
 
 console.log('This is the JavaScript entry file - your code begins here.');
