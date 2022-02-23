567b79e952e39c9f234c5f1d1a42d8f74db4c2a0
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-22 18:23:23 -0700

    Add data set to sleep test

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -7,9 +7,10 @@ import Sleep from '../src/Sleep';
 describe('Sleep', () => {
   let sleep;
   let testSleep;
+  let userID2TestSleep;
 
   beforeEach(() => {
-    testSleep = [
+    userID2TestSleep = [
       { "userID": 2, "date": "2019/06/15", "hoursSlept": 7, "sleepQuality": 4.7 },
       { "userID": 2, "date": "2019/06/16", "hoursSlept": 7.5, "sleepQuality": 3.8 },
       { "userID": 2, "date": "2019/06/17", "hoursSlept": 5.7, "sleepQuality": 3 },
@@ -18,6 +19,24 @@ describe('Sleep', () => {
       { "userID": 2, "date": "2019/06/21", "hoursSlept": 4.3, "sleepQuality": 4.8 },
       { "userID": 2, "date": "2019/06/22", "hoursSlept": 4.8, "sleepQuality": 3.3 }
     ];
+
+    testSleep = [
+      { "userID": 2, "date": "2019/06/15", "hoursSlept": 7, "sleepQuality": 4.7 },
+      { "userID": 2, "date": "2019/06/16", "hoursSlept": 7.5, "sleepQuality": 3.8 },
+      { "userID": 2, "date": "2019/06/17", "hoursSlept": 5.7, "sleepQuality": 3 },
+      { "userID": 2, "date": "2019/06/18", "hoursSlept": 10.8, "sleepQuality": 3.2 },
+      { "userID": 2, "date": "2019/06/19", "hoursSlept": 9.6, "sleepQuality": 2.5 },
+      { "userID": 2, "date": "2019/06/21", "hoursSlept": 4.3, "sleepQuality": 4.8 },
+      { "userID": 2, "date": "2019/06/22", "hoursSlept": 4.8, "sleepQuality": 3.3 },
+      { "userID": 1, "date": "2019/06/15", "hoursSlept": 6.1, "sleepQuality": 2.2 },
+      { "userID": 1, "date": "2019/06/16", "hoursSlept": 4.1, "sleepQuality": 3.8 },
+      { "userID": 1, "date": "2019/06/17", "hoursSlept": 8, "sleepQuality": 2.6 },
+      { "userID": 1, "date": "2019/06/18", "hoursSlept": 10.4, "sleepQuality": 3.1 },
+      { "userID": 1, "date": "2019/06/19", "hoursSlept": 10.7, "sleepQuality": 1.2 },
+      { "userID": 1, "date": "2019/06/20", "hoursSlept": 9.3, "sleepQuality": 1.2 },
+      { "userID": 1, "date": "2019/06/21", "hoursSlept": 7.8, "sleepQuality": 4.2 }
+    ];
+
   sleep = new Sleep(testSleep, 2);
   });
 
@@ -26,7 +45,7 @@ describe('Sleep', () => {
   })
 
   it('should be able to take in an array of objects that is the sleepData', () => {
-    expect(sleep.sleepData).to.eql(testSleep)
+    expect(sleep.sleepData).to.eql(userID2TestSleep)
   })
 
   it('should have a parameter that is a user id', () => {
