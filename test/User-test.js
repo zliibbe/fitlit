1ab0acf6fb8f4f6a6870f7eeebda069a73e7f890
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-22 18:22:35 -0700

    Refactor User tests to allign with refactored methods

--- test/User-test.js
+++ test/User-test.js
@@ -134,11 +134,11 @@ describe('User', () => {
     });
 
     it('should have a method that returns the total daily hours slept', () => {
-      expect(user.totalAvgDailyHoursSlept(testSleep, 2)).to.equal(7.1)
+      expect(user.totalAvgDailyHoursSlept(sleep.sleepData, 2)).to.equal(7.1)
     })
 
     it('should have a method that calculates the average sleep quality per day over all time', () => {
-      expect(user.avgAllTimeSleepQuality(testSleep, 2)).to.equal(3.61)
+      expect(user.avgAllTimeSleepQuality(sleep.sleepData)).to.equal(3.61)
     })
 
     it('should have a method that returns the sleep quality for a specific day', () => {
@@ -150,7 +150,7 @@ describe('User', () => {
     })
 
     it('should have a method that displays the daily hours slept in a week', () => {
-      expect(user.sevenDaysOfHoursSlept(testSleep, ["2019/06/15", "2019/06/16","2019/06/17","2019/06/18","2019/06/19","2019/06/20","2019/06/21","2019/06/22", 2])).to.equal(7, 7.5, 5.7, 10.8, 9.6, 4.3, 4.8) 
+      expect(user.sevenDaysOfHoursSlept(testSleep, ["2019/06/15", "2019/06/16","2019/06/17","2019/06/18","2019/06/19","2019/06/20","2019/06/21","2019/06/22", 2])).to.equal(7, 7.5, 5.7, 10.8, 9.6, 4.3, 4.8)
     })
 
     it('should have a method that displays the daily quality of hours slept in a week', () => {
