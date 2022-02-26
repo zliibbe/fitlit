3872ced3379b9bccbdc03aba43f4f616405e43c6
Zach Liibbe <zliibbe@gmail.com>
2022-02-26 15:30:10 -0700

    Add POST framework and refactor fetch calls to be more DRY

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,11 @@
-const userDataURL = "http://localhost:3001/api/v1/users";
-const sleepDataURL = "http://localhost:3001/api/v1/sleep";
-const hydrationDataURL = "http://localhost:3001/api/v1/hydration";
-const activityDataURL = "http://localhost:3001/api/v1/activity"
+// const userDataURL = "http://localhost:3001/api/v1/users";
+// const sleepDataURL = "http://localhost:3001/api/v1/sleep";
+// const hydrationDataURL = "http://localhost:3001/api/v1/hydration";
+// const activityDataURL = "http://localhost:3001/api/v1/activity"
 
 let errorMsg = document.querySelector('.error-display');
 
-const fetchData = (url) => { return fetch(url).then(response => {
+const fetchData = (dataType) => { return fetch(`http://localhost:3001/api/v1/${dataType}`).then(response => {
     if (!response.ok) {
         throw new Error('Error with requesting information.');
       } else {
@@ -18,4 +18,25 @@ const fetchData = (url) => { return fetch(url).then(response => {
     })
 }
 
-export {fetchData, userDataURL, sleepDataURL, hydrationDataURL, activityDataURL}
+const postData = (dataType) => {
+    fetch(`http://localhost:3001/api/v1/${dataType}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newAnimal)
+    })
+        .then((response) => {
+        if (!response.ok) {
+            throw new Error('422 Error: Server is not currently running')
+        } else {
+            return response.json()
+        }
+    })
+    .then((animal) => addAnimalToPage(animal))
+    .catch(error => {
+        error = 'TypeError: Failed to fetch' ? errorMsg.innerText = 'Server is not currently running' : 
+        errorMsg.innerText = 'Error encountered. See console.'
+        console.warn(error)
+    })
+}
+
+export {fetchData, postData}
