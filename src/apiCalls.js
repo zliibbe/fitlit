25a134414afbb1448f020d67562508c9f6608528
Zach Liibbe <zliibbe@gmail.com>
2022-02-25 23:09:03 -0700

    Add error handling for fetch calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,6 +2,15 @@ const userDataURL = "https://fitlit-api.herokuapp.com/api/v1/users";
 const sleepDataURL = "https://fitlit-api.herokuapp.com/api/v1/sleep";
 const hydrationDataURL = "https://fitlit-api.herokuapp.com/api/v1/hydration";
 
-const fetchData = (url) => fetch(url).then(response => response.json())
+const fetchData = (url) => { return fetch(url).then(response => {
+    if (!response.ok) {
+        throw new Error('Error with requesting information.');
+      } else {
+       return response.json();
+      }
+    }).catch(error => {
+        console.warn(error)
+    })
+}
 
-export {fetchData, userDataURL, sleepDataURL, hydrationDataURL};
+export {fetchData, userDataURL, sleepDataURL, hydrationDataURL}
