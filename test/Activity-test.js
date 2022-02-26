56c30b7ad11c9ae7c98e3daa208c5ad32c07befd
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-26 14:48:28 -0700

    Set up describe block for activity tests

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -2,3 +2,11 @@ import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
 import User from '../src/User';
 import Activity from '../src/Activity';
+
+describe('Activity', () => {
+  let activity;
+  let testActivity;
+  let testActivitybyUserID;
+
+  
+})
