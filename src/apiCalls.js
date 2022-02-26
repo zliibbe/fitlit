ef24442b90563dd35d5df542eb17a423149cfe01
Zach Liibbe <zliibbe@gmail.com>
2022-02-26 10:47:17 -0700

    Add error querySelector and error to .catch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,6 +3,8 @@ const sleepDataURL = "http://localhost:3001/api/v1/sleep";
 const hydrationDataURL = "http://localhost:3001/api/v1/hydration";
 const activityDataURL = "http://localhost:3001/api/v1/activity"
 
+let errorMsg = document.querySelector('.error-display');
+
 const fetchData = (url) => { return fetch(url).then(response => {
     if (!response.ok) {
         throw new Error('Error with requesting information.');
@@ -10,6 +12,8 @@ const fetchData = (url) => { return fetch(url).then(response => {
        return response.json();
       }
     }).catch(error => {
+        error = 'TypeError: Failed to fetch' ? errorMsg.innerText = 'Server is not currently running' : 
+        errorMsg.innerText = 'Error encountered. See console.'
         console.warn(error)
     })
 }
