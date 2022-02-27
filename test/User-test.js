956e212467183b4f2c7a405cd21c0cc833bcb855
Andrew Musselman <andrew.mussels@gmail.com>
2022-02-26 20:21:42 -0700

    Add tests for User to test activity data

--- test/User-test.js
+++ test/User-test.js
@@ -3,6 +3,7 @@ import UserRepository from '../src/UserRepository';
 import User from '../src/User';
 import Hydration from '../src/Hydration';
 import Sleep from '../src/Sleep';
+import Activity from '../src/Activity';
 
 describe('User', () => {
   let testUsers;
@@ -155,6 +156,74 @@ describe('User', () => {
 
     it('should have a method that displays the daily quality of hours slept in a week', () => {
       expect(user.getSevenDaysOfSleepQuality(sleep, ["2019/06/15", "2019/06/16","2019/06/17","2019/06/18","2019/06/19","2019/06/20","2019/06/21","2019/06/22"], 2)).to.eql([4.7, 3.8, 3, 3.2, 2.5, 4.8, 3.3])
+    })
+  })
+  describe('activity', () => {
+    let activity;
+    let testActivity;
+    let testActivitybyUserID;
+    let dataByDate;
+    let dates;
+
+    beforeEach(() => {
+      dates = ["2019/06/15", "2019/06/16", "2019/06/17", "2019/06/18", "2019/06/19", "2019/06/20", "2019/06/21"]
+      testActivitybyUserID = [
+        { "userID": 1, "date": "2019/06/15", "numSteps": 3577, "minutesActive": 140, "flightsOfStairs": 16 },
+        { "userID": 1, "date": "2019/06/16", "numSteps": 6637, "minutesActive": 175, "flightsOfStairs": 36 },
+        { "userID": 1, "date": "2019/06/17", "numSteps": 14329, "minutesActive": 168, "flightsOfStairs": 18 },
+        { "userID": 1, "date": "2019/06/18", "numSteps": 4419, "minutesActive": 165, "flightsOfStairs": 33 },
+        { "userID": 1, "date": "2019/06/19", "numSteps": 8429, "minutesActive": 275, "flightsOfStairs": 2 },
+        { "userID": 1, "date": "2019/06/20", "numSteps": 14478, "minutesActive": 140, "flightsOfStairs": 12 },
+        { "userID": 1, "date": "2019/06/21", "numSteps": 6760, "minutesActive": 135, "flightsOfStairs": 6 },
+        { "userID": 1, "date": "2019/06/22", "numSteps": 10289, "minutesActive": 119, "flightsOfStairs": 6 }
+      ];
+
+      testActivity = [
+        {"userID": 2, "date": "2019/06/15", "numSteps": 4294, "minutesActive": 138, "flightsOfStairs": 10},
+        {"userID": 2, "date": "2019/06/16", "numSteps": 4112, "minutesActive": 220, "flightsOfStairs": 37},
+        {"userID": 2, "date":"2019/06/17", "numSteps":13750, "minutesActive": 65, "flightsOfStairs": 4},
+        {"userID": 2,"date": "2019/06/18", "numSteps": 4662, "minutesActive": 181, "flightsOfStairs": 31},
+        {"userID": 2, "date": "2019/06/19", "numSteps": 9858, "minutesActive": 243, "flightsOfStairs": 44},
+        {"userID": 2, "date": "2019/06/20", "numSteps": 8153, "minutesActive": 74, "flightsOfStairs": 10},
+        {"userID": 2, "date": "2019/06/21", "numSteps": 10225, "minutesActive": 174, "flightsOfStairs": 26},
+        {"userID": 2, "date": "2019/06/22", "numSteps": 3605, "minutesActive": 124, "flightsOfStairs": 31},
+        {"userID": 1, "date": "2019/06/15", "numSteps": 3577, "minutesActive": 140, "flightsOfStairs": 16 },
+        {"userID": 1, "date": "2019/06/16", "numSteps": 6637, "minutesActive": 175, "flightsOfStairs": 36 },
+        {"userID": 1, "date": "2019/06/17", "numSteps": 14329, "minutesActive": 168, "flightsOfStairs": 18 },
+        {"userID": 1, "date": "2019/06/18", "numSteps": 4419, "minutesActive": 165, "flightsOfStairs": 33 },
+        {"userID": 1, "date": "2019/06/19", "numSteps": 8429, "minutesActive": 275, "flightsOfStairs": 2 },
+        {"userID": 1, "date": "2019/06/20", "numSteps": 14478, "minutesActive": 140, "flightsOfStairs": 12 },
+        {"userID": 1, "date": "2019/06/21", "numSteps": 6760, "minutesActive": 135, "flightsOfStairs": 6 },
+        {"userID": 1, "date": "2019/06/22", "numSteps": 10289, "minutesActive": 119, "flightsOfStairs": 6 }
+        ];
+
+      dataByDate = [
+        {"userID": 2, "date": "2019/06/16", "numSteps": 4112, "minutesActive": 220, "flightsOfStairs": 37},
+        {"userID": 1, "date": "2019/06/16", "numSteps": 6637, "minutesActive": 175, "flightsOfStairs": 36 }
+        ];
+
+      activity = new Activity(testActivity, 1);
+
+    })
+
+    it('Should return the miles a user has walked for a specific date', () => {
+      expect(user.getMilesByDate("2019/06/15")).to.equal(15381.1)
+    })
+    it('Should should be able to tell how many minutes a user was active for a specific date', () => {
+      expect(user.getMinutesActiveByDate("2019/06/21")).to.equal(135)
+    })
+    it('Should return the average minutes active for a given week', () => {
+      expect(user.getAvgMinutesActiveByWeek(dates)).to.equal(1193)
+    })
+    it('Should tell a user if they met their step goal for a specific day', () => {
+      expect(user.getStepGoalMet("2019/06/17")).to.be(true)
+      expect(user.getStepGoalMet("2019/06/21")).to.be(false)
+    })
+    it('Should find all the days a user met their step goal', () => {
+      expect(user.getAllStepGoalsMet(testActivitybyUserID)).to.eql(["2019/06/17", "2019/06/20", "2019/06/22"])
+    })
+    it('Should be able to find the day with the highest number of stairs climbed', () => {
+      expect(user.getStairClimbingRecord(testActivitybyUserID)).to.equal("2019/06/16")
     })
   })
 })
