368fdeb848dd2bcd223bacbcaaa6e0449335d49a
Zach Liibbe <zliibbe@gmail.com>
2022-02-27 21:53:14 -0700

    Add post calls for sleep and hydration data forms

--- src/scripts.js
+++ src/scripts.js
@@ -15,7 +15,15 @@ let sleepUser;
 
 let moreInfoBtn = document.querySelector('#more-user-info');
 let enterNewDataBtn = document.querySelector('.add-new-data');
-let backToMainBtn = document.querySelector('.back-to-main')
+let backToMainBtn = document.querySelector('.back-to-main');
+let submitSleep = document.querySelector('.sleep-inputs');
+let submitHydro = document.querySelector('.hydration-inputs');
+let sleepDateInput = document.querySelector('.sleep-date-input');
+let sleepHoursInput = document.querySelector('.sleep-hours-input');
+let sleepQualityInput = document.querySelector('.sleep-quality-input');
+let hydroDateInput = document.querySelector('.hydration-date-input');
+let hydroNumOzInput = document.querySelector('.hydration-num-oz-input');
+
 
 //functions
 const getRandomNumber = (num) => {
@@ -36,6 +44,31 @@ const getTheData = (id) => {
   .then(() => domUpdates.generateSleepCard(sleepUser, ourUser))
 }
 
+const postNewSleepData = (e) => {
+  e.preventDefault();
+  // const formData  = new FormData(e.target);
+  const newSleepData = {
+    userID: ourUser.id,
+    date: sleepDateInput.value.replaceAll('-', '/'),
+    hoursSlept: parseInt(sleepHoursInput.value),
+    sleepQuality: parseInt(sleepQualityInput.value),
+  }
+  postData("sleep", newSleepData);
+  e.target.reset();
+}
+
+const postNewHydroData = (e) => {
+  e.preventDefault();
+  // const formData  = new FormData(e.target);
+  const newHydroData = {
+    userID: ourUser.id,
+    date: hydroDateInput.value.replaceAll('-', '/'),
+    numOunces: parseInt(hydroNumOzInput.value)
+  }
+  postData("hydration", newHydroData);
+  e.target.reset();
+}
+
 //Data/DOM; initial function on Load
 const loadUserInfo = () => {
   getTheData(getRandomNumber(50));
@@ -46,4 +79,5 @@ window.addEventListener('load', loadUserInfo)
 moreInfoBtn.addEventListener('click', domUpdates.infoButton)
 enterNewDataBtn.addEventListener('click', domUpdates.toggleDataDisplay)
 backToMainBtn.addEventListener('click', domUpdates.toggleDataDisplay)
-
+submitSleep.addEventListener('submit', postNewSleepData)
+submitHydro.addEventListener('submit', postNewHydroData)
